91a1011241e469b31c8ed9a5ba295efa4707915b
atete847-prog <atete847@gmail.com>
2026-01-31 00:13:34 -0300

    Delete herzai directory

--- herzai/analizador_app.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from analizador_sentimientos import analizar_sentimientos
-
-app = FastAPI()
-
-@app.get("/sentimiento")
-def sentimiento(texto: str):
-    return analizar_sentimientos(texto)
